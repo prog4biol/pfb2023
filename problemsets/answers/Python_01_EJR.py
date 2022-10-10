4ad2d6e0d02074901fb47e325bb2a727add8d3ad
Eric Ross <ejr@stowers.org>
2022-10-10 18:01:39 -0500

    add Eric's problem sets answers for python_1 and python_2

---
+++ problemsets/answers/Python_01_EJR.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# ejr: 2022-10-10
+# pfb 2022: problem set python 2
+
+### IMPORTS
+import sys
+
+### MAIN
+
+
+# static
+activity = "Reading"
+animal = "Platypus"
+
+
+print("Static output:")
+print("My name: Eric")
+print("My favorite color: Gray")
+print("My favorite activity: ", activity)
+print("My favorite activity: ", animal)
+
+
+# command line 
+print("\n\n")
+print("From Command Line:")
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print("My name: " + name)
+print("My favorite color: " + color)
+print("My favorite activity: " + activity)
+print("My favorite activity: " + animal)
